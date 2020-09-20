f7a1a3d94146ca1e43c668e2004453f72c007689
Eliezer <eliezer1789j@gmail.com>
2020-09-20 22:36:56 +0100

    components life cycle

--- src/components/ClassComponent/ClassComponent.tsx
+++ src/components/ClassComponent/ClassComponent.tsx
@@ -2,11 +2,24 @@ import React from 'react'
 import './ClassComponent.css'
 
 class ClassComponent extends React.Component {
+    constructor(props : any) {
+        super(props)
+        console.log('constructor reached.')
+    }
     state = {
         name: 'Mundo'
     }
 
+    componentDidMount() {
+        console.log('did mount reached.')
+    }
+
+    componentDidUpdate() {
+        console.log('did update reached.')
+    }
+
     render() {
+        console.log('render reached.')
         return <div className='ClassComponent'>
             <h5>I am a class based component!</h5>
             <p>name: { this.state.name }</p>
