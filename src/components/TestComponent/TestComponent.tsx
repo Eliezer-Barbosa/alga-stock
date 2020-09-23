a59f764642e8170dad6258bb4b347c8b730cde3c
Eliezer <eliezer1789j@gmail.com>
2020-09-23 14:34:22 +0100

    add year state hook

--- src/components/TestComponent/TestComponent.tsx
+++ src/components/TestComponent/TestComponent.tsx
@@ -3,15 +3,19 @@ import './TestComponent.css'
 
 function TestComponent (props: { name: string }) {
     const [age, setAge] = useState(33)
+    const [year, setYear] = useState(2020-age)
     return <div className="TestComponent">
         Test Component Works { props.name }! <br />
         <button onClick={() => {
             setAge(age - 1)
+            setYear(year + 1)
         }}>-</button>
         AGE: { age }
         <button onClick={() => {
             setAge(age + 1)
+            setYear(year - 1)
         }}>+</button>
+        <br /> YEAR: { year }
     </div>
 }
 
