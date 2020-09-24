6ef838156f7ac0c0d0121d84ba3d05a42bacd881
Eliezer <eliezer1789j@gmail.com>
2020-09-24 14:10:18 +0100

    clear up whole application

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,28 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import TestComponent from '../TestComponent'
-import ClassComponent from '../ClassComponent'
 
 function App() {
   return (
     <div className="App">
-      <TestComponent name = 'Barbosa!!'/>
-      <ClassComponent/>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      App
     </div>
   );
 }
