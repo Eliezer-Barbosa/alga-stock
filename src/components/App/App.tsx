7b423d5d250a8f7e0134f6334a0f0876559de745
Eliezer <eliezer1789j@gmail.com>
2020-09-25 14:44:33 +0100

    clear app and add table component's base files

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,34 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Header from '../Header';
-import Button from '../../shared/Button'
 import './App.css';
 import Container from '../../shared/Container';
-import Input from '../../shared/Input';
-
-/**
- * component created to demonstrate
- * how to pass a component as prop
- */
-function TestComponent () {
-  return <img width='30px' src="https://images.macrumors.com/t/5-2nCNbY-U3lZFgBOFUw0yLFzus=/400x0/filters:quality(90)/article-new/2017/11/crying-tears-of-joy-emoji-250x248.jpg?lossy" alt="emoji"/>
-}
 
 function App() {
-  const [street, setStreet] = useState('')
   return (
     <div className="App">
       <Header title='AlgaStock'/>
 
       <Container>
-       <ul>
-         {
-           ['Daniel', 'William', 'Thiago', 'Daniel'].map((name, index) => {
-             return <li key={index}>
-               { `${name} ${index}` }
-             </li>
-           })
-         }
-       </ul>
+        
       </Container>
       
     </div>
