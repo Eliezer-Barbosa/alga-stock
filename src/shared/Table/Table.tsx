81ebd7bc2b6593ff001929b7d6a054ff68a70183
Eliezer <eliezer1789j@gmail.com>
2020-09-25 16:21:10 +0100

    add sass package and style table with sass

--- src/shared/Table/Table.tsx
+++ src/shared/Table/Table.tsx
@@ -1,30 +1,30 @@
 import React from 'react'
-// import './Table.scss'
+import './Table.scss'
 
 const Table = () => {
-    return <table>
+    return <table className="AppTable">
         <thead>
             <tr>
                 <th>Product</th>
                 <th>Price</th>
-                <th>Stock</th>
+                <th className="right">Stock</th>
             </tr>
         </thead>
         <tbody>
             <tr>
                 <td>Paella Seafood</td>
                 <td>12.95</td>
-                <td>5</td>
+                <td className="right">5</td>
             </tr>
             <tr>
                 <td>Chorizo al vino</td>
                 <td>10.50</td>
-                <td>4</td>
+                <td className="right">4</td>
             </tr>
             <tr>
                 <td>Grated Mahon</td>
                 <td>8.90</td>
-                <td>5</td>
+                <td className="right">5</td>
             </tr>
         </tbody>
 
