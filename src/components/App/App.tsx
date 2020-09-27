710299887c550b8098e9362c38c584ec4ff97c24
Eliezer <eliezer1789j@gmail.com>
2020-09-27 14:41:29 +0100

    using props in table

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,7 +2,15 @@ import React from 'react';
 import Header from '../Header';
 import './App.css';
 import Container from '../../shared/Container';
-import Table from '../../shared/Table';
+import Table, { TableHeader } from '../../shared/Table';
+import Products from '../../shared/Table/Table.mockdata';
+
+const headers: TableHeader[] = [
+  { key: 'id', value: '#' },
+  { key: 'name', value: 'Product' },
+  { key: 'price', value: 'Price', right: true },
+  { key: 'stock', value: 'Available Stock', right: true }
+]
 
 function App() {
   return (
@@ -10,7 +18,9 @@ function App() {
       <Header title='AlgaStock'/>
 
       <Container>
-        <Table />
+        <Table
+          headers = { headers }
+          data = { Products }  />
       </Container>
       
     </div>
