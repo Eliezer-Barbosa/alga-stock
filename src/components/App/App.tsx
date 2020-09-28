162534990d33878cc53e708c49578774eb6226a3
Eliezer <eliezer1789j@gmail.com>
2020-09-28 16:33:25 +0100

    add form product component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,53 +4,25 @@ import './App.css';
 import Container from '../../shared/Container';
 import Table, { TableHeader } from '../../shared/Table';
 import Products from '../../shared/Table/Table.mockdata';
-import Form from '../../shared/Form';
-import Input from '../../shared/Input';
-import Button from '../../shared/Button';
+import ProductForm from '../Products/ProductForm';
 
 const headers: TableHeader[] = [
   { key: 'id', value: '#' },
   { key: 'name', value: 'Product' },
   { key: 'price', value: 'Price', right: true },
-  { key: 'stock', value: 'Available Stock', right: true }
-]
+  { key: 'stock', value: 'Available Stock', right: true },
+];
 
 function App() {
   return (
     <div className="App">
-      <Header title='AlgaStock'/>
+      <Header title="AlgaStock" />
 
       <Container>
-        <Table
-          headers = { headers }
-          data = { Products }  
-        />
+        <Table headers={headers} data={Products} />
 
-        <Form title="Product Form" onSubmit={console.log}>
-          <Input 
-            label="Name"
-            placeholder="E.g.: Cookie"
-          />
-          <Input 
-            label="Price"
-            type="number"
-            step="0.01"
-            min="0"
-            placeholder="E.g.: 1.25"
-          />
-          <Input 
-            label="Stock"
-            type="number"
-            min="0"
-            placeholder="E.g.: 15"
-          />
-          <Button>
-            Submit
-          </Button>
-        </Form>
-
+        <ProductForm />
       </Container>
-      
     </div>
   );
 }
