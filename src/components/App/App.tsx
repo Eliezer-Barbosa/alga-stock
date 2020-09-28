074436249ee346cafcea6ea56d3cbd40ea5d9297
Eliezer <eliezer1789j@gmail.com>
2020-09-28 23:34:38 +0100

    form reset

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -15,6 +15,7 @@ const headers: TableHeader[] = [
 
 function App() {
   const [products, setProducts] = useState(Products);
+  const [updatingProduct, setUpdatingProduct] = useState<Product | undefined>(products[0])
 
   const handleProductSubmit = (product: ProductCreator) => {
     setProducts([
@@ -32,6 +33,8 @@ function App() {
         product.id === newProduct.id ? newProduct : product
       )
     );
+
+    setUpdatingProduct(undefined)
   };
 
   return (
@@ -42,7 +45,7 @@ function App() {
         <Table headers={headers} data={products} />
 
         <ProductForm
-          form={products[0]}
+          form={updatingProduct}
           onSubmit={handleProductSubmit}
           onUpdate={handleProductUpdate}
         />
