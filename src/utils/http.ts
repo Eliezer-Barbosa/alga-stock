8baa5a87872ac0358394e5236e88b1705992da98
Eliezer <eliezer1789j@gmail.com>
2020-10-08 12:45:00 +0100

    add redux and product reducer

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,14 +1,7 @@
-import axios from 'axios';
+import axios from 'axios'
 
 const http = axios.create({
-<<<<<<< HEAD
-  baseURL: 'http://localhost:4010',
-  headers: {
-    authorization: 'Bearer 123',
-  },
-=======
-  baseURL: 'http://localhost:3024',
->>>>>>> 04-02-installing-redux
-});
+  baseURL: 'http://localhost:3024'
+})
 
-export default http;
+export default http
