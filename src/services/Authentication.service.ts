74faaac6c0238ee49c5d8be81652698e76a8d408
Eliezer <eliezer1789j@gmail.com>
2020-10-26 21:53:28 +0000

    fix bug login

--- src/services/Authentication.service.ts
+++ src/services/Authentication.service.ts
@@ -1,17 +1,17 @@
 import http from '../utils/http';
 
-export declare interface User {
+export interface User {
   _id: string;
   user: string;
   email: string;
   role: 'admin' | 'customer';
   token: string;
   createdAt: string;
-  updatedAT: string;
+  updatedAt: string;
 }
 
-export const signInUser = (user: string, pass: string) => {
+export const signInUser = (user: string, pass: string) => 
   http
     .post<User>('/authentication/login', { user, pass })
-    .then((res) => res.data);
-};
+    .then(res => res.data);
+    
